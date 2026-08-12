18e45efbd6639bd170224d9264b48f79cbf65d47
xyz-harshal <harshal011kamble@gmail.com>
2026-08-12 14:32:17 +0530

    codegen/emitter done and wrote some unit tests on that

---
+++ src/codegen/emitter.rs
@@ -0,0 +1,35 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Emitter {
+    buf: Vec<u8>,
+}
+
+impl Emitter {
+    pub fn new() -> Self {
+        Self {
+            buf: Vec::new(),
+        }
+    }
+    pub fn emit_ret(&mut self) {
+        self.buf.push(0xC3);
+    }
+    pub fn emit_mov_rax_imm64(&mut self, val: i64) {
+        self.buf.push(0x48); //REX.W prefix for using the x86_32 opcode in x86_64 architecture.
+        self.buf.push(0xB8); //The opcode for MOV
+        self.buf.extend_from_slice(&val.to_le_bytes()); //pushing the values in an little endian way.
+    }
+    pub fn finish(self) -> Vec<u8> {
+        self.buf
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::codegen::emitter::Emitter;
+    #[test]
+    fn mov_number() {
+        let mut emitter = Emitter::new();
+        emitter.emit_mov_rax_imm64(69);
+        emitter.emit_ret();
+        assert_eq!(emitter.finish(), [0x48, 0xB8, 69, 0, 0, 0, 0, 0, 0, 0, 0xC3]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,18 @@ use frontend::parser::parse;
 mod backend;
 use backend::interpreter::eval;
 
+mod codegen;
+use codegen::emitter::Emitter;
+
 fn main() {
     let content: String = std::fs::read_to_string("input.txt").unwrap();
     match parse(&content) {
         Ok(expr) => {
             println!("{:?}", eval(&expr));
+            let mut emitter = Emitter::new();
+            emitter.emit_mov_rax_imm64(eval(&expr));
+            emitter.emit_ret();
+            println!("{:?}", emitter.finish());
         },
         Err(err) => println!("Error: {}", err),
     }
